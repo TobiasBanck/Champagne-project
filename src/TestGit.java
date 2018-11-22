e2382f6b93323b43f3bc82c1ff181d71a7e86174
joakim deak <joakimdeak@hotmail.com>
2018-11-22 12:00:13 +0100

    something

--- src/TestGit.java
+++ src/TestGit.java
@@ -1,5 +1,5 @@
 public class TestGit {
     public static void main(String[] args) {
-        System.out.println("testing, change this text or make a new source code and push ");
+        System.out.println("testing, change this text or make a new source code and push");
     }
 }
